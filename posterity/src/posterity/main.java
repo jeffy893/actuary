5607b8f263e6dd07b94e6d069cc2934b4472adb4
Jefferson Richards <jefferson.f.richards@gmail.com>
2020-03-13 12:47:49 -0700

    Posterity outputs up to 3 graphs

--- posterity/src/posterity/main.java
+++ posterity/src/posterity/main.java
@@ -17,18 +17,14 @@ public class main {
 		// TODO Auto-generated method stub
 		
 		
-		double time = 0.0;
 		double flux_people = 20.0;
 		double num_people = 100.0;
-		double pace = 0.1667; //Minutes of average song divided by 240.0
-		double heat = 0.83; // Subj probability from 0 to 1 that there will be dancing
+		double pace = 0.1; //Minutes of average song divided by 240.0
+		double heat = 0.83; //Subj probability from 0 to 1 that there will be dancing
 		int intervals = (int) (4.0/pace); //Number of songs in 4 hours
-		double heatdelta = 1.0/(4.0/pace); // For incrementing the heat variable at the end of each song
-		int size = 3; // Groups of 6, 14, and 24 will fight it out to end up bison or cattle
-		double wisdom = 0.12; // This is gaussian for both sides but a necessary variable in the lanchester
-		
-		//System.out.println(intervals);
-		//System.out.println(heatdelta);
+		double heatdelta = 1.0/(4.0/pace); //For incrementing the heat variable at the end of each song
+		int size = 3; //Groups of 6, 14, and 24 will fight it out to end up bison or cattle
+		double wisdom = 0.12; //This is gaussian for both sides but a necessary variable in the lanchester
 		
 		
 		// This is the size of group that should be approached
@@ -95,61 +91,110 @@ public class main {
 		Set<double[][]> graphs = raw_battle.getGraphs(cohc, actual.heatcount, actual.bdchain, size, flux_people, cohdelta, forcedelta);
 		
 		
+		boolean oneneutral = false;
+		boolean oneepic = false;
+		boolean oneunexpected = false;
+		
+		
 		for(double[][] graph : graphs) {
 			
 			double[][] bison = new double[100][2];
 			double[][] cattle = new double[100][2];
 			
-			boolean death1 = false;
-			boolean death2 = false;
 			
-			for(int i=0; i < 100; i++) {
+			
+			
+			boolean neutral1 = false;
+			boolean neutral2 = false;
+			boolean unexpected = false;
+			boolean bisonmax1 = false;
+			boolean bisonmax2 = false;
+			boolean epic = false;
+			if(graph[1][0] > graph[1][1]) {
+				bisonmax1 = true;
+			}else {
+				bisonmax1 = false;
+			}
+			
+			bison[0][1] = size;
+			cattle[0][1] = size;
+			
+			for(int i=1; i < 100; i++) {
 				bison[i][0] = i;
 				cattle[i][0] = i;
 				bison[i][1] = graph[i][0];
 				cattle[i][1] = graph[i][1];
 				
-				if(graph[i][0] < 0 || graph[i][1] < 0) {
-					death1 = true;
-				}
-				if(graph[i][0] < 0 && graph[i][1] < 0) {
-					death2 = true;
+				
+				if(unexpected == true && epic == false) {
+					if(bisonmax2 == true && graph[i][1] > graph[i][0]) {
+						epic = true;
+					}
+					if(bisonmax2 == false && graph[i][1] < graph[i][0]) {
+						epic = true;
+					}
 				}
 				
+				if(unexpected == false) {
+					if(bisonmax1 == true && graph[i][1] > graph[i][0]) {
+						unexpected = true;
+						bisonmax2 = false;
+					}
+					if(bisonmax1 == false && graph[i][1] < graph[i][0]) {
+						unexpected = true;
+						bisonmax2 = true;
+					}
+				}
+				
+				
+				
+				if(graph[i][0] < 0 || graph[i][1] < 0) {
+					neutral1 = true;
+				}
+				if(graph[i][0] < 0 && graph[i][1] < 0) {
+					neutral2 = true;
+				}
+				if(graph[i][0] > size || graph[i][1] > size) {
+					neutral1 = true;
+				}
+				Double nanb = graph[i][0];
+				Double nanc = graph[i][1];
+				if(nanb.isNaN() || nanc.isNaN()) {
+					neutral2 = true;
+				}
 				
 				
 			}
 			
-			if(death1 == false && death2 == false) {
-				for(int i=0; i < 100; i++) {
-					System.out.println(i + "," + bison[i][1] + "," + cattle[i][1]);
+			
+			if(neutral2 == false) {
+				if(neutral1 == false && oneneutral == false){
+					System.out.println("Neutral");
+					for(int i=0; i < 100; i++) {
+						
+						System.out.println(i + "," + bison[i][1] + "," + cattle[i][1]);
+					}
+					oneneutral = true;
+				}
+				if(epic == true && oneepic == false) {
+					System.out.println("Epic");
+					for(int i=0; i < 100; i++) {
+						
+						System.out.println(i + "," + bison[i][1] + "," + cattle[i][1]);
+					}
+					oneepic = true;
+				}
+				if(unexpected == true && oneunexpected == false) {
+					System.out.println("Unexpected");
+					for(int i=0; i < 100; i++) {
+						
+						System.out.println(i + "," + bison[i][1] + "," + cattle[i][1]);
+					}
+					oneunexpected = true;
 				}
 			}
 			
 		}
-		
-		
-		//double[][] bdchain = new double[intervals][intervals];
-		
-		// Number of Steps likely to end up with all cattle or all bison
-		
-		
-		
-		//System.out.println(raw_heats);
-		
-		// System of Lanchester 2. Returns values for Bison[.get(0)], Cattle[.get(1)]
-		
-		
-		//ArrayList<Double[]> graph = raw_battle.getGraph();
-		
-		
-		//for(int i = 0; i <= intervals; i++) {
-			
-			//System.out.println(graph.get(0)[i]);
-			//System.out.println(graph.get(1)[i]);
-			
-		//}
-		
 		
 
 	}
